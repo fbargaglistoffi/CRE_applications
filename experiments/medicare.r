c71d76bb7baeadfd3d6f516f9aa76a79381b18fc
Riccardo Cadei <47679062+riccardocadei@users.noreply.github.com>
2023-01-26 19:02:24 -0500

    remove cutoff

--- experiments/medicare.r
+++ experiments/medicare.r
@@ -1,25 +1,48 @@
-library("dplyr")
-library("CRE")
+library(devtools)
+library(dplyr)
+library(SuperLearner)
+library(CRE)
 set.seed(2022)
 
 statename <- "united_states"
-t_pm25 <- 10
+t_pm25 <- 8
 outcome <- "mean"
 zip <- FALSE
-q <- 0
-cutoff <- 0.9
-pfer <- 5
+q <- 0.1
+pfer <- 10
+estimator <- "aipw"
+n_cores <- 8
+
 
 # Load (preprocessed) Medicare
 setwd(paste("/n/home_fasse/rcadei/medicare/",statename,sep=''))
-load("medicare.RData")
-X <- select(medicare, -dead_in_5, -pm25, -followup_year_plus_one, -pm25_12)
-z <- ifelse((medicare$pm25 <= t_pm25), 0, 1)
-y <- medicare$dead_in_5
-outcome <- "unit"
+if (zip){
+  load("medicare_zip.RData")
+  medicare_zip <- medicare_zip[medicare_zip$log_dead!=-Inf,]
+  X <- select(medicare_zip, -dead_in_5, -pm25)
+  z <- ifelse((medicare_zip$pm25 <= t_pm25), 0, 1)
+  if (outcome=="mean"){
+    y <- medicare_zip$dead_in_5
+  } else if (outcome=="log_mean"){
+    y <- log(medicare_zip$dead_in_5)
+  } else if (outcome=="logit_mean") {
+    y <- log(medicare_zip$dead_in_5/(1-medicare_zip$dead_in_5))
+  } else {
+    stop("Unkown Outcome. Please select between 'mean', 'loag_mean' or 'logit_mean'.")
+  }
+} else {
+  load("medicare.RData")
+  X <- select(medicare, -dead_in_5, -pm25)
+  z <- ifelse((medicare$pm25 <= t_pm25), 0, 1)
+  y <- medicare$dead_in_5
+  outcome <- "unit"
+}
 
+m_xgboost <- function(nthread = n_cores, ...) {
+  SuperLearner::SL.xgboost(nthread = nthread, ...)
+}
 if (q>0) {
-  ps_hat <- estimate_ps(z, X, "SL.xgboost")
+  ps_hat <- estimate_ps(z, X, "m_xgboost")
   lower_q <- quantile(ps_hat, q)
   upper_q <- quantile(ps_hat, 1-q)
   X <- X[which(ps_hat >= lower_q & ps_hat <= upper_q),]
@@ -27,34 +50,34 @@ if (q>0) {
   y <- y[which(ps_hat >= lower_q & ps_hat <= upper_q)]
 }
 
+
 intervention_vars <- c('male', 'white', 'black', 'hispanic', 'other_race',
-                       'age', 'dual', 'medhouseholdincome', 'medianhousevalue',
-                       'mean_bmi', 'smoke_rate', 'poverty', 'education',
-                       'popdensity', 'pct_blk','pct_owner_occ')
+                       'old', 'medicaid', 'zc_household_income', 'zc_house_value',
+                       'county_bmi', 'county_smoke', 'zc_poverty', 'zc_nodiploma',
+                       'zc_density', 'zc_black','zc_hispanic','zc_owner_occupancy')
 
 # Set Parameters
 method_params = list(ratio_dis = 0.5,
-                     ite_method_dis = "slearner",
-                     ps_method_dis = "SL.xgboost",
-                     ps_method_inf = "SL.xgboost",
-                     oreg_method_dis = "SL.xgboost",
-                     oreg_method_inf = "SL.xgboost",
-                     ite_method_inf = "slearner")
+                     ite_method_dis = estimator,
+                     ps_method_dis = "m_xgboost",
+                     ps_method_inf = "m_xgboost",
+                     oreg_method_dis = "m_xgboost",
+                     oreg_method_inf = "m_xgboost",
+                     ite_method_inf = estimator)
 
 hyper_params = list(intervention_vars = intervention_vars,
                     offset = NULL,
-                    ntrees_rf = 40,
-                    ntrees_gbm = 40,
+                    ntrees_rf = 100,
+                    ntrees_gbm = 100,
                     node_size = 20,
                     max_nodes = 4,
                     max_depth = 2,
-                    replace = TRUE,
-                    t_decay = 0.01,
+                    replace = FALSE,
+                    t_decay = 0.5,
                     t_ext = 0.005,
                     t_corr = 0.5,
                     t_pvalue = 0.05,
                     stability_selection = TRUE,
-                    cutoff = cutoff,
                     pfer = pfer,
                     penalty_rl = 1)
 
@@ -65,7 +88,7 @@ print(paste("Time:",round((time_end - time_start)[[3]],2),"sec"))
 
 summary(cre_result)
 
-exp_name <- paste("CRE_pm25",t_pm25,outcome,"q",q,"pfer",pfer,sep="_")
+exp_name <- paste("CRE_pm25",t_pm25,outcome,estimator,"pfer",pfer,sep="_")
 pdf(paste(exp_name,".pdf",sep=""), width = 6.5, height = 4)
 plot(cre_result)
 dev.off()
